822352e78443bff6afc647755c6e60e8c884e496
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-10-11 15:53:57 -0300

    new front end project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function redi(nomepagina){
+    window.location.href = `file:///C:/Users/Joelma/desenvolvimento-lucas/projetofrontend/${nomepagina}.html`;
+}
+
+function error() {
+    alert('Função indisponível no momento!');
+}
+
+function projeto() {
+    alert('Esse é um projeto de aprendizado feito por Lucas Oliveira. No momento não existe outras funções! Obrigado por participar desse projeto ;)');
+}
